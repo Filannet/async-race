539b059ebef9acc11d65282723a22f47ff42c248
Anna Filatova <filanna33@gmail.com>
2024-04-22 00:50:08 +0400

    feat: setup base project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Garage from "./pages/garage/Garage";
+import Winners from "./pages/winners/Winners";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Garage />,
+    },
+    {
+        path: "/garage",
+        element: <Garage />,
+    },
+    {
+        path: "/winners",
+        element: <Winners />,
+    },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/garage/Garage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Garage.css';
+
+function Garage() {
+  return (
+    <div>
+      <h1>Garage</h1>
+    </div>
+  );
+}
+
+export default Garage;
